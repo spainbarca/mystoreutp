0ee4d2e89cb8d59ec2ba74042243253173e8ba6f
spainbarca <69886685+spainbarca@users.noreply.github.com>
2020-11-10 20:34:33 -0500

    Update ruta.controlador.php

--- controladores/ruta.controlador.php
+++ controladores/ruta.controlador.php
@@ -4,13 +4,13 @@ class ControladorRuta{
 
 	static public function ctrRuta(){
 
-		return "http://localhost:8083/MyStore/";
+		return "https://spainbarca.github.io/mystoreutp/";
 
 	}
 
 	static public function ctrServidor(){
 
-		return "http://localhost:8083/MyStore/backend/";
+		return "https://spainbarca.github.io/mystoreutp/backend/";
 	}
 
 }
