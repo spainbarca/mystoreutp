f5b7781cf608d57db03ed98c4a00f20e7df81887
spainbarca <69886685+spainbarca@users.noreply.github.com>
2020-11-10 20:32:06 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ Class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=reservas-hotel",
-						"root",
-						"");
+		$link = new PDO("mysql:host=sql203.ezyro.com;dbname=ezyro_27165654_mystoreutp",
+						"ezyro_27165654",
+						"8gdob8vvl93h");
 
 		$link->exec("set names utf8");
 
